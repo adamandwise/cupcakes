ffc84c30290f9fd1874a96a77b2784c04d3c7ba1
Adam Wise <wise.adam@student.greenriver.edu>
2023-01-05 17:15:50 -0500

    created a cupcake fundraiser ordering form

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cup Cake Fundraiser</title>
+</head>
+<body>
+    <h1>Cupcake Fundraiser</h1>
+    <form>
+        <label>Your Name:</label><br>
+        <input type="textbox" value="name"><br>
+        <label>Cupcake flavors</label><br>
+        <input type="checkbox" id="flavor1" name="flavor1" value="grasshopper">
+        <label for="flavor1">The Grasshopper</label><br>
+        <input type="checkbox" id="flavor2" name="flavor2" value="whiskey">
+        <label for="flavor2">Whiskey Maple Bacon</label><br>
+        <input type="checkbox" id="flavor3" name="flavor3" value="carrotwalnut">
+        <label for="flavor3">Carrot Walnut</label><br>
+        <input type="checkbox" id="flavor4" name="flavor4" value="saltedcarmel">
+        <label for="flavor4">Salted Carmel Cupcake</label><br>
+        <input type="checkbox" id="flavor5" name="flavor5" value="redvelvet">
+        <label for="flavor5">Red Velvet</label><br>
+        <input type="checkbox" id="flavor6" name="flavor6" value="lemondrop">
+        <label for="flavor6">Lemon Drop</label><br>
+        <input type="checkbox" id="flavor7" name="flavor7" value="tiramisu">
+        <label for="flavor7">Tiramisu</label><br>
+
+        <input type="submit" value="order">
+    </form>
+</body>
+</html>
